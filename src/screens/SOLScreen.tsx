abf2b4c81d0348841a12e7f313e4e399edeac5f7
Yeonhan Jung <yt4307@gmail.com>
2023-08-08 21:49:39 +0900

    feat: Home screen 이외의 screen에서는 헤더를 다크모드로 고정하는 기능 추가

--- src/screens/SOLScreen.tsx
+++ src/screens/SOLScreen.tsx
@@ -1,15 +1,29 @@
-import { Dimensions, View, Text, StatusBar } from "react-native";
+import { useCallback } from "react";
+import { Dimensions, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useDispatch } from "react-redux";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { styled } from "styled-components/native";
+
+import { setTheme } from "redux/slice/themeSlice";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("screen");
 
 const SOLScreen = () => {
   const insets = useSafeAreaInsets();
+
+  const dispatch = useDispatch();
+
+  useFocusEffect(
+    useCallback(() => {
+      dispatch(setTheme({ dark: true }));
+      return () => {}; // 필요한 경우 cleanup 함수 추가
+    }, [])
+  );
+
   return (
     <>
-      <StatusBar barStyle={"dark-content"} />
       <StatusBarBackgroundColor height={insets.top} />
       <Container paddingTop={insets.top}>
         <Text>SOL Screen</Text>
