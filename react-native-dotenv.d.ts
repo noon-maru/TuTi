0881fb0fdbf0f4297bc14083a96aca4c874b425b
Yeonhan Jung <yt4307@gmail.com>
2023-08-13 18:47:37 +0900

    update: .env type 수정 - .env는 무조건 string으로 값을 가져온다

--- react-native-dotenv.d.ts
+++ react-native-dotenv.d.ts
@@ -1,6 +1,6 @@
 declare module "@env" {
   export const SERVER_URL: string;
   export const DEVELOP_SERVER_URL: string;
-  export const DEVELOP_MODE: boolean;
+  export const DEVELOP_MODE: string;
   export const API: string;
 }
