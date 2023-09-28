119a04a56d20ba5bdefe5c3b4f6009e5f9b924ff
Yeonhan Jung <yt4307@gmail.com>
2023-09-28 22:01:36 +0900

    feat: box 스크린 기능 임시 추가

--- src/screens/BoxScreen.tsx
+++ src/screens/BoxScreen.tsx
@@ -1,7 +1,9 @@
 import { Dimensions, Text } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import styled from "styled-components/native";
+import CourseTabContent from "components/Tour/CourseTabContent";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("screen");
 
@@ -12,7 +14,27 @@ const BoxScreen = () => {
     <>
       <StatusBarBackgroundColor height={insets.top} />
       <Container>
-        <Text>Box Screen</Text>
+        <MainContentsContainer>
+          <TabContentsContainer
+            contentContainerStyle={{ gap: 10 }}
+            bounces={false}
+            showsVerticalScrollIndicator={false}
+            scrollEventThrottle={16}
+          >
+            <CourseTabContent
+              title={"A 코스"}
+              duration={"TIME 30 ~ 40"}
+            ></CourseTabContent>
+            <CourseTabContent
+              title={"B 코스"}
+              duration={"TIME 10 ~ 20"}
+            ></CourseTabContent>
+            <CourseTabContent
+              title={"C 코스"}
+              duration={"TIME 40 ~ 60"}
+            ></CourseTabContent>
+          </TabContentsContainer>
+        </MainContentsContainer>
       </Container>
     </>
   );
@@ -30,6 +52,55 @@ const Container = styled.View`
   align-items: center;
 
   margin-top: 53px;
+`;
+
+const SearchBoxContainer = styled.View`
+  position: relative;
+  justify-content: flex-end;
+  align-items: center;
+
+  width: ${SCREEN_WIDTH}px;
+  height: 51px;
+`;
+
+const MainContentsContainer = styled.View`
+  flex: 1;
+
+  width: 100%;
+
+  padding: 25px;
+`;
+
+const TabButtonContainer = styled.View`
+  flex-direction: row;
+  gap: 10px;
+`;
+
+const TabButton = styled(LinearGradient)`
+  width: 100%;
+  padding: 6px 10px;
+
+  border-radius: 10px;
+`;
+
+const TabContentsContainer = styled.ScrollView`
+  flex: 1;
+
+  margin-top: 8px;
+`;
+
+const Submit = styled.Pressable`
+  align-self: flex-end;
+
+  justify-content: center;
+  align-items: center;
+
+  width: 90px;
+  height: 25px;
+
+  border-radius: 10px;
+
+  background-color: gray;
 `;
 
 export default BoxScreen;
