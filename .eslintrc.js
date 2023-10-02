1392ce16d34de6281a6240999d850e160c82d522
Yeonhan Jung <yt4307@gmail.com>
2023-10-02 21:10:01 +0900

    styles: eslint 및 prettier 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,24 @@
 module.exports = {
   root: true,
   extends: ["@react-native"],
+  // Prettier 설정 추가
+  // plugins: ["prettier"],
   rules: {
     "arrow-parens": ["warn", "always"],
     quotes: ["warn", "double"],
     "react/react-in-jsx-scope": "off",
     "react-native/no-inline-styles": "off",
     curly: "off",
+
+    // "prettier/prettier": [
+    //   "error",
+    //   {
+    //     arrowParens: "always",
+    //     bracketSameLine: false,
+    //     bracketSpacing: true,
+    //     singleQuote: false,
+    //     trailingComma: "es5",
+    //   },
+    // ],
   },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   arrowParens: "always",
-  bracketSameLine: true,
+  bracketSameLine: false,
   bracketSpacing: true,
   singleQuote: false,
-  trailingComma: "all",
+  trailingComma: "es5",
 };
