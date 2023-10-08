135cab327a4d9af6769cf02ce305a9496fb5c1ac
Yeonhan Jung <yt4307@gmail.com>
2023-10-08 22:04:12 +0900

    chore: 폰트 크기 기본값 변경 - 15px

--- src/styles/globalStyles.tsx
+++ src/styles/globalStyles.tsx
@@ -2,10 +2,12 @@ import { styled } from "styled-components/native";
 
 export const StyledText = styled.Text`
   font-family: "SpoqaHanSansNeo-Regular";
+  font-size: 15px;
   color: black;
 `;
 
 export const BoldStyledText = styled.Text`
   font-family: "SpoqaHanSansNeo-Bold";
+  font-size: 15px;
   color: black;
 `;
