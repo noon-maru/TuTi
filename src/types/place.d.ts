a973084316581ce34f6b25e5449c92094db7bb8d
Yeonhan Jung <yt4307@gmail.com>
2023-10-15 02:48:18 +0900

    type: Place, Tag 타입 정의

---
+++ src/types/place.d.ts
@@ -0,0 +1,9 @@
+interface Place {
+  _id: string;
+  region: string;
+  name: string;
+  address: string;
+  image: string;
+  numberHearts: number;
+  tags: tag[];
+}
---
+++ src/types/tag.d.ts
@@ -0,0 +1,5 @@
+interface tag {
+  tagName: string;
+  associatedCount: number;
+  relatedTags: any; // 관련 태그의 오브젝트id
+}
